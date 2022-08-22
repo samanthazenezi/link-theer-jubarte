f2ba7546e4a1c3ba8e453aec44d383210a739727
Samantha Zenezi <samanthazenezi@gmail.com>
2022-08-22 19:06:05 -0300

    links baleia jubarte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImagemComponent } from './imagem/imagem.component';
+import { LinksComponent } from './links/links.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeitoporComponent } from './feitopor/feitopor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagemComponent,
+    LinksComponent,
+    FooterComponent,
+    FeitoporComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feitopor/feitopor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feitopor',
+  templateUrl: './feitopor.component.html',
+  styleUrls: ['./feitopor.component.css']
+})
+export class FeitoporComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
